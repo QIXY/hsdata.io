c2063a65f60f5ac35d24a1ce2b3693c607e53786
QIXY <qixiaoyu77@126.com>
2023-09-05 15:33:10 +0800

    Update modelnetwork.js

--- modelnetwork.js
+++ modelnetwork.js
@@ -8,7 +8,7 @@ var dom = document.getElementById('modelnetwork');
     var option;
 
     myChart.showLoading();
-$.getJSON('./data/network.json', function (graph) {
+$.get('./data/network.json', function (graph) {
   myChart.hideLoading();
   graph.nodes.forEach(function (node) {
     node.label = {
